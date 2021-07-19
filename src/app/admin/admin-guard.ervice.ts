160401fb7926bb19a6b67f72851c9c6305ee5ad8
Kristi <kristi.martiko@fshnstudent.info>
2021-07-19 15:43:50 +0200

    Admin Guard Added

---
+++ src/app/admin/admin-guard.ervice.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate ,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot,
+    Router,
+    UrlTree} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable()
+
+export class AdminGuard implements CanActivate {
+
+    constructor(private authService: AuthService,
+        private router: Router) {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        if(this.authService.getAdmin() == true) {
+            return true;
+        } else {
+            this.router.navigate(['/home']);
+            return false;
+        }
+    }
+}
